d7e8ddce13ad1f3faa70cf072bb7a6703d810ec7
Prakhar Jain <prakharj05@gmail.com>
2020-12-05 15:02:54 +0530

    firebase update

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import * as firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDNuiMykLkGQM3kaOZTf3UKwKBw8ATZWN8",
-  authDomain: "todoist-3f99b.firebaseapp.com",
-  databaseURL: "https://todoist-3f99b.firebaseio.com",
-  projectId: "todoist-3f99b",
-  storageBucket: "todoist-3f99b.appspot.com",
-  messagingSenderId: "656714702669",
-  appId: "1:656714702669:web:6204ba5c040bac8c62e104"
+  apiKey: "AIzaSyC8lCK33rHpyukFviNIjb_cKkRzme0EW6g",
+  authDomain: "todoist-d346b.firebaseapp.com",
+  databaseURL: "https://todoist-d346b.firebaseio.com",
+  projectId: "todoist-d346b",
+  storageBucket: "todoist-d346b.appspot.com",
+  messagingSenderId: "213064210983",
+  appId: "1:213064210983:web:73255e166299b5631ccc02",
 };
 
 firebase.initializeApp(firebaseConfig);
